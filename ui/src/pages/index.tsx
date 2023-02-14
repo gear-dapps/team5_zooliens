b3525ebfad472c792b0731da4570997d675fc422
clearloop <26088946+clearloop@users.noreply.github.com>
2023-02-14 22:38:18 +0300

    feat(ui): basic layout for all pages

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,11 +1,46 @@
-import styles from "@/styles/page.module.css";
+import profileStyles from "@/styles/profile.module.css";
+import Zoomlie from "@/components/Zoomlie";
+import Farm from "@/components/Farm";
+import { useState } from "react";
+
+const navigation = [
+  { name: "Zoomlies", current: false },
+  { name: "Farm", current: false },
+];
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Profile() {
+  const [isZoomlies, setIsZoomlies] = useState(true);
+
   return (
     <>
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Page Profile</h1>
-      </main>
+      <div className={profileStyles.nav}>
+        {navigation.map((item) => (
+          <div
+            key={item.name}
+            className={classNames(
+              item.current
+                ? "bg-gray-900 text-white"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+              "block px-3 py-2 rounded-md text-base font-medium"
+            )}
+            aria-current={item.current ? "page" : undefined}
+            onClick={() => {
+              if (item.name === "Zoomlies" && !isZoomlies) {
+                setIsZoomlies(true);
+              } else if (item.name === "Farm" && isZoomlies) {
+                setIsZoomlies(false);
+              }
+            }}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+      {isZoomlies ? <Zoomlie /> : <Farm />}
     </>
   );
 }
