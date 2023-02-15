782157f38c7feec6ac986c62a90c985c1cf98d69
Ivan Azoyan <azoyan.89@mail.ru>
2023-02-15 10:56:04 +0300

    add NFT to tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,12 +1,17 @@
 use app_io::{Action, Create, Error, Event, Mint};
 use ed25519_compact::{KeyPair, Seed};
-use gtest::{Log, Program, System};
+use gtest::{Log, Program, RunResult, System};
+use nft_io::*;
+
+use gear_lib::non_fungible_token::token::{TokenId, TokenMetadata};
 
 const SEED: [u8; 32] = [5u8; 32];
 
 const DATA: [u8; 32] = [9u8; 32];
 
-const USER: [u64; 6] = [4, 5, 6, 7, 8, 9];
+const USERS: [u64; 6] = [10, 11, 12, 13, 14, 15];
+
+const NFT_PATH: &str = "target/nft-0.2.5.opt.wasm";
 
 #[test]
 fn create() {
@@ -15,29 +20,29 @@ fn create() {
     system.init_logger();
 
     let program = Program::current(&system);
-    let mut result = program.send_bytes(2, []);
+    let result = program.send_bytes(USERS[0], []);
 
     assert!(!result.main_failed());
 
-    let key_pair = KeyPair::from_seed(Seed::new(SEED));
+    for (i, user) in USERS.iter().enumerate() {
+        init_nft(&system, *user, format!("Zoomlien {}", *user));
+        let key_pair = KeyPair::from_seed(Seed::new(SEED));
+        let secret_key = key_pair.sk;
 
-    let public_key = key_pair.pk;
-    let secret_key = key_pair.sk;
+        let signature = secret_key.sign(&DATA, None);
 
-    let signature = secret_key.sign(&DATA, None);
+        let payload = Create {
+            signature: signature.to_vec(),
+            signed_data: DATA.to_vec(),
+        };
 
-    let payload = Create {
-        signature: signature.to_vec(),
-        signed_data: DATA.to_vec(),
-    };
+        let action = Action::Create(payload);
 
-    let action = Action::Create(payload);
+        let result = program.send(*user, action);
 
-    let result = program.send(0, action);
+        assert!(result.contains(&Log::builder().payload(Event::Created(i as u64))));
+    }
 
-    // assert!(result.contains(&Log::builder().payload( Event::Created(0))));
-
-    assert!(result.contains(&Log::builder().payload(Event::Created(0))));
     // assert!(result.log().is_empty());
 }
 
@@ -48,47 +53,49 @@ fn mint() {
     system.init_logger();
 
     let program = Program::current(&system);
-    let mut result = program.send_bytes(USER[0], []);
+    let result = program.send_bytes(USERS[0], []);
 
     assert!(!result.main_failed());
 
-    let key_pair = KeyPair::from_seed(Seed::new(SEED));
+    for (i, user) in USERS.iter().enumerate() {
+        init_nft(&system, *user, format!("Zoomlien {}", *user));
+        let key_pair = KeyPair::from_seed(Seed::new(SEED));
+        let secret_key = key_pair.sk;
 
-    let public_key = key_pair.pk;
-    let secret_key = key_pair.sk;
+        let signature = secret_key.sign(&DATA, None);
 
-    let signature = secret_key.sign(&DATA, None);
+        let payload = Create {
+            signature: signature.to_vec(),
+            signed_data: DATA.to_vec(),
+        };
 
-    let payload = Create {
-        signature: signature.to_vec(),
-        signed_data: DATA.to_vec(),
-    };
+        let action = Action::Create(payload);
 
-    let action = Action::Create(payload);
+        let result = program.send(*user, action);
 
-    let result = program.send(USER[1], action);
-    assert!(result.contains(&Log::builder().payload(Event::Created(0))));
+        assert!(result.contains(&Log::builder().payload(Event::Created(i as u64))));
 
-    let result = program.send(
-        USER[1],
-        Action::Mint(Mint {
-            id: 0,
-            private_key: secret_key.to_vec(),
-        }),
-    );
+        let result = program.send(
+            USERS[1],
+            Action::Mint(Mint {
+                id: i as u64,
+                private_key: secret_key.to_vec(),
+            }),
+        );
 
-    assert!(result.contains(&Log::builder().payload(Event::Minted)));
+        assert!(result.contains(&Log::builder().payload(Event::Minted)));
+    }
 }
 
-pub fn init_nft(sys: &System, owner: u64) {
+pub fn init_nft(sys: &System, owner: u64, name: String) {
     let nft_program = Program::from_file(sys, "./target/nft-0.2.5.opt.wasm");
 
     let res = nft_program.send(
         owner,
         nft_io::InitNFT {
-            name: String::from("MyToken"),
-            symbol: String::from("MTK"),
-            base_uri: String::from(""),
+            name,
+            symbol: String::from("ZML"),
+            base_uri: String::from(""), // link to image
             royalties: None,
         },
     );
@@ -127,4 +134,26 @@ pub fn init_nft(sys: &System, owner: u64) {
     );
 
     assert!(!res.main_failed());
+}
+
+#[allow(dead_code)]
+pub fn nft_owner(nft_program: &Program, from: u64, token_id: TokenId) -> RunResult {
+    nft_program.send(from, nft_io::NFTAction::Owner { token_id })
+}
+
+pub fn init(sys: &System) -> Program {
+    USERS
+        .iter()
+        .for_each(|user| sys.mint_to(*user, 1_000_000_000));
+    let owner_user = USERS[0];
+
+    sys.init_logger();
+
+    let auction_program = Program::current(sys);
+
+    auction_program.send(owner_user, ());
+
+    init_nft(sys, owner_user, "her".to_string());
+
+    auction_program
 }
